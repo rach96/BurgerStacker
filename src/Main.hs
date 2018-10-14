0c6ff0d02fe163ce281807f8b81d6002de63e944
Rachel Sunderland <rachsunderland@gmail.com>
2018-10-14 13:56:38 -0700

    second commit

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,11 @@
 module Main (main) where
 
 import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Data.Bitmap
 import Codec.Picture
 import Graphics.Gloss.Juicy
+import Data.Maybe
 
 -- | Data describing the state of the Burger game.
 data BurgerStacker = Game
@@ -10,12 +13,51 @@ data BurgerStacker = Game
     , burgerVel :: (Float, Float) -- ^ Burger (x,y) velocity
     } deriving Show
 
+data BurgerSprites = Sprites {
+  bunSprite :: Picture
+}
+
 -- | The starting state for the game of BurgerStacker.
 initialState :: BurgerStacker
 initialState = Game
   { burgerLoc = (0, -100)
   , burgerVel = (1, -3)
   }
+
+loadPicture :: IO BurgerSprites
+loadPicture = do
+  picture <- loadJuicyPNG "/Users/rachelsunderland/Desktop/burger_sandwich_PNG4160.png"
+  return Sprites {
+    bunSprite = fromJust picture
+  }
+
+renderBun :: BurgerSprites -> Picture
+renderBun = do
+  bunSprite
+
+-- | Convert a game state into a picture
+render :: BurgerStacker -> Picture 
+render game = renderBun
+  
+-- | Update the game by moving the ball.
+-- Ignore the ViewPort argument.
+update :: ViewPort -> Float -> BurgerStacker -> BurgerStacker 
+update _ = moveBurger 
+
+moveBurger :: Float -> BurgerStacker -> BurgerStacker
+moveBurger seconds game = game 
+  where
+    -- Old locations and velocities.
+    (x, y) = burgerLoc game
+    (vx, vy) = burgerVel game
+
+    -- New locations.
+    x' = x + vx * seconds
+    y' = y + vy * seconds
+
+-- | Number of frames to show per second
+fps :: Int
+fps = 60
 
 width, height, offset :: Int
 width = 500
@@ -31,11 +73,38 @@ background = white
 default_img :: Picture
 default_img = Circle 80
 
+getPicture :: IO (Maybe Picture)
+getPicture = do
+  loadJuicyPNG "/Users/rachelsunderland/Desktop/burger_sandwich_PNG4160.png"
+
+main :: IO ()
+main = do
+  simulate window background fps initialState render update
+
+   
+{- 
+pictureIO :: Picture
+pictureIO = do 
+  picture <- loadJuicyPNG "/Users/rachelsunderland/Desktop/burger_sandwich_PNG4160.png"
+  case picture of 
+    Nothing -> default_img
+    (Just i) -> i 
+
 main :: IO ()
 main = do
     picture <- loadJuicyPNG "/Users/rachelsunderland/Desktop/burger_sandwich_PNG4160.png"
     case picture of 
         Nothing -> display window background default_img
         (Just i) -> display window background (translate (0) (-100) $ color (dark red) $ i)
+
+main :: IO ()
+main = animate window background frame 
+  where
+    frame :: Float -> Picture
+    frame seconds = render $ moveBurger seconds initialState -}
+
+
+
+
 
 
